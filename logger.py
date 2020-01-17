dd29ccdcae6448d4530a9e2edccb6d6102fc695a
Reet Nayan <47450107+ReetNayan@users.noreply.github.com>
2020-01-17 07:52:13 +0530

    Added comments on Logger.py...
    Added some more information about the program.

--- logger.py
+++ logger.py
@@ -1,23 +1,34 @@
 # Program which adds logs with timestamp.
+# e.g.
+# ---Log Start---
+# Current Time & Date
+# - - - - - - - -
+# #Your log
+# #...
+# ---Log End---
 
 import time
 
 time = time.asctime()
 
-file = open('Daily_Log.txt' , 'a+')
+file = open('Daily_Log.txt' , 'a+') # Create a new file if not exists and open to append data
 
-timestamp = "---Log Start--- \n"+time+"\n- - - - - - - - - \n"
+timestamp = "---Log Start--- \n"+time+"\n- - - - - - - - - \n" # Add the Date and time
 
-file.write(timestamp)
+file.write(timestamp) # Write the timestamp to file
 
+# Instructions for user...
 print("Write your notes and log here.")
 print("A timestamp will be added before your new log entry")
 print("After writing a sentence hit return to add new lines.")
+print()
 print("- Want to exit and save?")
 print("- Hit return for a new line, type 'end' and hit")
 print("  return again to save and exit.")
 
 x = input('Hit return and start writing.')
+
+# A loop which keeps asking for Log data to write and will stop only when 'end' is entered.
 while True:
     data = input()
     if data == "end":
